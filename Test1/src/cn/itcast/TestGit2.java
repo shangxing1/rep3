9006372e2b765ce292feb95e9e5fd1b90512345e
root <root@itheima.com>
2020-09-07 10:03:34 +0800

    新增Test2

---
+++ Test1/src/cn/itcast/TestGit2.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("new new ");
+    }
+}
